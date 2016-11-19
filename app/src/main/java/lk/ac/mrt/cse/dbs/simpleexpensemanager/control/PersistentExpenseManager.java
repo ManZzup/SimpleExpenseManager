0ec7cca1d1ae656781dba54291771341d7f86eea
manzzup <manujith.nc@gmail.com>
2016-11-19 18:33:36 +0530

    RESET

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -11,21 +11,7 @@ public class PersistentExpenseManager extends ExpenseManager {
     public void setup(){
         SQLiteDatabase mydatabase = SQLiteDatabase.openOrCreateDatabase("expenses",null);
 
-        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Account(" +
-                "Account_no VARCHAR PRIMARY KEY," +
-                "Bank VARCHAR," +
-                "Holder VARCHAR," +
-                "Initial_amt REAL" +
-                " );");
-
-        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Transactions(" +
-                        "Transaction_id INTEGER PRIMARY KEY," +
-                        "Account_no VARCHAR" +
-                        "Type INT2," +
-                        "Amt REAL," +
-                        "Date DATE," +
-                        "FOREIGN KEY (Account_no) REFERENCES Account(Account_no)" +
-                ");");
+        
 
     }
 }
