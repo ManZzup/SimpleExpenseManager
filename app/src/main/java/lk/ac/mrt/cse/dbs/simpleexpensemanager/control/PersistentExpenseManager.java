4c3455ef4b5869c1f69e8a4184fa022b3a64dc33
manzzup <manujith.nc@gmail.com>
2016-11-19 18:33:53 +0530

    Step 01 - creating tables

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -11,7 +11,21 @@ public class PersistentExpenseManager extends ExpenseManager {
     public void setup(){
         SQLiteDatabase mydatabase = SQLiteDatabase.openOrCreateDatabase("expenses",null);
 
-        
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Account(" +
+                "Account_no VARCHAR PRIMARY KEY," +
+                "Bank VARCHAR," +
+                "Holder VARCHAR," +
+                "Initial_amt REAL" +
+                " );");
+
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Transactions(" +
+                "Transaction_id INTEGER PRIMARY KEY," +
+                "Account_no VARCHAR" +
+                "Type INT2," +
+                "Amt REAL," +
+                "Date DATE," +
+                "FOREIGN KEY (Account_no) REFERENCES Account(Account_no)" +
+                ");");
 
     }
 }
