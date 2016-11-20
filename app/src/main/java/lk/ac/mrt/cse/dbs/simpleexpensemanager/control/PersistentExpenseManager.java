b1faeae91e521922f61a333a02748cadfd0dc23a
manzzup <manujith.nc@gmail.com>
2016-11-20 10:13:32 +0530

    Fixed Step 01

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -1,15 +1,26 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
+import android.app.Application;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
 
 
 /**
  * Created by manujith on 11/15/16.
  */
 public class PersistentExpenseManager extends ExpenseManager {
+    private Context ctx;
+    public PersistentExpenseManager(Context ctx){
+        this.ctx = ctx;
+        setup();
+    }
     @Override
     public void setup(){
-        SQLiteDatabase mydatabase = SQLiteDatabase.openOrCreateDatabase("expenses",null);
+        SQLiteDatabase mydatabase = ctx.openOrCreateDatabase("expenses5", ctx.MODE_PRIVATE, null);
 
         mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Account(" +
                 "Account_no VARCHAR PRIMARY KEY," +
@@ -18,14 +29,19 @@ public class PersistentExpenseManager extends ExpenseManager {
                 "Initial_amt REAL" +
                 " );");
 
-        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Transactions(" +
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS TransactionLog(" +
                 "Transaction_id INTEGER PRIMARY KEY," +
-                "Account_no VARCHAR" +
-                "Type INT2," +
+                "Account_no VARCHAR," +
+                "Type INT," +
                 "Amt REAL," +
-                "Date DATE," +
+                "Log_date DATE," +
                 "FOREIGN KEY (Account_no) REFERENCES Account(Account_no)" +
                 ");");
 
+        AccountDAO accountDAO = new PersistentAccountDAO(mydatabase);
+
+        setAccountsDAO(accountDAO);
+
+        setTransactionsDAO(new PersistentTransactionDAO(mydatabase));
     }
 }
---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -0,0 +1,7 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+/**
+ * Created by manujith on 11/19/16.
+ */
+public class PersistentTransactionDAO {
+}
